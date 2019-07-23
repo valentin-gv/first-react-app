479de8e000765b934eb68258f04190e60b2f41d1
valentin-gv <valentin.gavela@despegar.com>
2019-07-23 17:51:26 -0300

    remove bodyParser

--- server/index.js
+++ server/index.js
@@ -1,39 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3200;
-const data = require('./mocks/data');
+const data = require("./mocks/data");
 const usersRouter = express.Router();
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.send('Welcome to my api') 
+app.get("/", (req, res) => {
+  res.send("Welcome to my api");
 });
 
-usersRouter.route('/civilizations')
-    .get((req, res) => {
-        res
-        .status(200)
-        .json(data.civilizations);
-    })
-    .post((req, res) => {
-        console.log(req.body);
-        res
-        .status(201)
-        .json(req.body);
-    });    
+usersRouter
+  .route("/civilizations")
+  .get((req, res) => {
+    res.status(200).json(data.civilizations);
+  })
+  .post((req, res) => {
+    console.log(req.body);
+    res.status(201).json(req.body);
+  });
 
-usersRouter.route('/users:userId')
-    .get((req, res) => {
-        // make filter by userId and response with one user
-        const userId = req.params.userId;
-        res.json(data.users);
-    });
+usersRouter.route("/users:userId").get((req, res) => {
+  // make filter by userId and response with one user
+  const userId = req.params.userId;
+  res.json(data.users);
+});
 
-app.use('/api', usersRouter);
+app.use("/api", usersRouter);
 
 app.listen(port, () => {
-    console.log('Running on port ' + port); 
+  console.log("Running on port " + port);
 });
